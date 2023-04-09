d1a1ba429d0248d9524d907f3d62624be419cccf
Roberto Jimenez <robertojimenezg@gmail.com>
2023-04-08 23:50:40 -0600

    Metodos de clase para manejar eventos

--- react-fundamentos/src/index.js
+++ react-fundamentos/src/index.js
@@ -5,21 +5,47 @@ class TarjetaFruta extends React.Component{
   constructor(){
     super()
 
+    //this.agregar = this.agregar.bind(this);
+    //this.quitar = this.quitar.bind(this);
+
+    // Para evitar hacer un bind por cada método a utilizar
+    const METHODS = ['agregar', 'quitar', 'limpiar'];
+    METHODS.forEach((method) => {
+      this[method] = this[method].bind(this);
+    })
+
     this.state = {
       cantidad : 0
     }
   }
+
+  agregar(){
+    this.setState({
+      cantidad : this.state.cantidad + 1
+    })
+  }
+
+  quitar(){
+    this.setState({
+      cantidad: this.state.cantidad - 1
+    })
+  }
+
+  limpiar(){
+    this.setState({
+      cantidad: 0
+    })
+  }
+
   render(){
     return (
       <div>
         <h3>{this.props.name}</h3>
         <hr />
         <div>Cantidad: {this.state.cantidad}</div>
-        <button 
-          onClick = {() => {
-            this.setState({cantidad : 10})
-          }}>Agregar
-        </button>
+        <button onClick = {this.agregar}> + </button>
+        <button onClick = {this.quitar}> - </button>
+        <button onClick = {this.limpiar}>limpiar</button>
         <p>$ {this.props.price}</p>
       </div>
     )
