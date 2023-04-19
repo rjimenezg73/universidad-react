43a10a0e2bdba9c1fe5198bc15e16afbb41296db
rjimenezg73 <robertojimenezg@gmail.com>
2023-04-19 17:48:47 -0600

    Manejando eventos de mouse

--- react-fundamentos/src/app.js
+++ react-fundamentos/src/app.js
@@ -1,6 +1,14 @@
-import React from 'react';
-import TarjetaFruta from './Componentes/TarjetaFruta';
+import React, {Component} from 'react';
+//import TarjetaFruta from './Componentes/TarjetaFruta';
 
+const styles = {
+  height: '200px',
+  background: 'gold',
+  padding: '1em',
+  boxSizing: 'border-box'
+}
+
+/*
 const App = () => (
   <div>
     <TarjetaFruta name='Sandía' price={5.00} />
@@ -8,5 +16,42 @@ const App = () => (
     <TarjetaFruta name='Kiwi' price={3.50} />
   </div>
 )
+*/
+
+class App extends Component{
+
+  state = {
+    x: 0,
+    y: 0,
+  }
+
+  manejador = (event) => {
+    this.setState({
+      x: event.clientX,
+      y: event.clientY,
+    })
+  }
+
+  render(){
+    return(
+      <div 
+        style={styles}
+        onMouseMove={this.manejador}
+      >
+        {/*<button
+          onClick={this.manejador}
+        >
+          Dispara
+        </button>*/}
+        <div>
+          x: {this.state.x}
+        </div>
+        <div>
+          y: {this.state.y}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
